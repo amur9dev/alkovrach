6bbffc68a61f2bde846f83db03107669b587511d
amur9dev <amur9dev@gmail.com>
2025-02-14 15:02:24 +0300

    added swiper

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.about__slider', {
+  loop: true,
+  slidesPerView: 1, 
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
+
+
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-const {src, dest, watch, parallel, series} = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss   = require('gulp-sass')(require('sass'));
+const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
@@ -10,45 +10,47 @@ const clean = require('gulp-clean');
 
 
 
-function scripts(){
+function scripts() {
   return src([
-    //'node_modules/swiper/swiper-bundle.js',
-    //...
+    'node_modules/swiper/swiper-bundle.min.js',
     'app/js/main.js',
   ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
-function images(){
+function images() {
   return src('app/images/**/*.*')
-  .pipe(imagemin([
-	    imagemin.gifsicle({interlaced: true}),
-	    imagemin.mozjpeg({quality: 75, progressive: true}),
-	    imagemin.optipng({optimizationLevel: 5}),
-	    imagemin.svgo({
-		    plugins: [
-			    {removeViewBox: true},
-			    {cleanupIDs: false}
-		    ]
-	    })
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
     ]))
-  .pipe(dest('dist/images'))
+    .pipe(dest('dist/images'))
 }
 
 
-function styles(){
-  return src('app/scss/style.scss')
-  .pipe(autoprefixer({ overrideBrowserlist: ['last 10 version']}))
-  .pipe(concat('style.min.css'))
-  .pipe(scss({outputStyle: 'compressed'}))
-  .pipe(dest('app/css'))
-  .pipe(browserSync.stream())
+function styles() {
+  return src([
+    'app/scss/style.scss',
+    'node_modules/swiper/swiper-bundle.min.css'
+  ])
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'] }))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
-function watching(){
+function watching() {
   watch(['app/scss/style.scss'], styles)
   watch(['app/js/main.js'], scripts)
   watch(['app/*.html']).on('change', browserSync.reload)
@@ -64,7 +66,7 @@ function browsersync() {
 
 function cleanDist() {
   return src('dist')
-  .pipe(clean())
+    .pipe(clean())
 }
 
 function building() {
@@ -72,8 +74,8 @@ function building() {
     'app/css/style.min.css',
     'app/js/main.min.js',
     'app/**/*.html'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('dist'))
 }
 
 exports.styles = styles;
