f4709fa225753f335b191c7780494a396f57e7b6
miz-developer <ya.cool-dmg@yandex.ru>
2025-02-20 20:44:23 +0300

    ad

--- app/js/main.js
+++ app/js/main.js
@@ -11,26 +11,23 @@ const swiper = new Swiper('.about__slider', {
 Fancybox.bind("[data-fancybox]", {});
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  const photoSlider = new Swiper(".personal__photo-slider", {
-    slidesPerView: 1, // Отображаем 3 фото
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-  });
-
-  const infoSlider = new Swiper(".personal__info-slider", {
-    slidesPerView: 1,
-    loop: true,
-    navigation: false,
-  });
-
-  // Синхронизация слайдеров
-  photoSlider.controller.control = infoSlider;
-  infoSlider.controller.control = photoSlider;
+const photoSlider = new Swiper('.personal__photo', {
+  slidesPerView: 3, 
+  loop: true, 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
 
+const infoSlider = new Swiper('.personal__info', {
+  slidesPerView: 1, 
+  loop: true, 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
+photoSlider.controller.control = infoSlider;
+infoSlider.controller.control = photoSlider;
